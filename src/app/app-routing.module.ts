421300285826480084a5c0867a41710c96d02b3b
Will保哥 <doggy.huang@gmail.com>
2017-08-27 14:53:56 +0800

    修復回到 Layout 時選單無法開啟的問題

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    enableTracing: true,
+    enableTracing: false,
     useHash: true
   })],
   exports: [RouterModule]
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -6,3 +6,22 @@ interface NodeModule {
 
 declare var $: any;
 declare var Colors: any;
+declare var Rickshaw;
+declare var Colors;
+declare var GMaps;
+declare var Ripple;
+declare var swal;
+declare var screenfull;
+declare var jQuery;
+declare var noUiSlider;
+declare var Dropzone;
+declare var APP_COLORS;
+declare var materialColors;
+declare var Datamap;
+interface Window {
+  JSON;
+  Ripple;
+  APP_COLORS;
+  Colors;
+  jQBrowser;
+}
