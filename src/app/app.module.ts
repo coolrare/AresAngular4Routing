37e1a0e0cb5b4b70aa45ba7d29fecd20bc901f8f
Will保哥 <doggy.huang@gmail.com>
2017-08-27 15:16:21 +0800

    匯入 FormsModule, ReactiveFormsModule 表單模組

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
@@ -23,6 +23,7 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule, ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [LoginGuard],
